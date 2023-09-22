04ab5689f9e2917a5d309eb0271d13119c2b53d4
younesse el-mouden <dayouness187@gmail.com>
2023-09-22 23:09:17 +0000

    4. Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# changes to our configuration file to a server without typing a password.
+file_line { 'Turn off passwd auth':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true
+}
+file_line { 'Delare identity file':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '     IdentityFile ~/.ssh/school',
+  replace => true
+}
