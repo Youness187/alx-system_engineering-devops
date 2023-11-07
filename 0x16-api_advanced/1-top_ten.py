02ae4ae42c98884bf462975de377e5e60edd0ca5
younesse el-mouden <dayouness187@gmail.com>
2023-11-07 18:45:00 +0000

    --

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -14,8 +14,7 @@ def top_ten(subreddit):
 
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "0x16-api_advanced:project:\
-        v1.0.0 (by /u/firdaus_cartoon_jr)"
+        "User-Agent": "0x16-api_advanced:project: v1.0.0 (by /u/dayouness)"
     }
     params = {
         "limit": 10
