13fbeda5fd2296ba7d82f4e56f204cfac3bcfb71
younesse el-mouden <dayouness187@gmail.com>
2023-11-07 18:29:43 +0000

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API and prints
-the titles of the first 10 hot posts listed for a given subreddit."""
+"""
+function that queries the Reddit API and prints
+the titles of the first 10 hot posts listed for a given subreddit
+"""
 import requests
 
 
