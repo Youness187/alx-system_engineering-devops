596d764d242b2ee187b7606048a90663b2082de8
younesse el-mouden <dayouness187@gmail.com>
2024-01-16 19:55:09 +0000

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,12 +7,22 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """function that queries the Reddit API
-       and returns the number of subscribers"""
-    if subreddit is None or type(subreddit) is not str:
+    """
+    function that queries the Reddit API
+    and returns the number of subscribers
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    headers = {"User-Agent": "MyBot/1.0 (your_username)"}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers_count = data["data"]["subscribers"]
+        return subscribers_count
+    elif response.status_code == 404:
+        print(f"Subreddit '{subreddit}' not found.")
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-                     v1.0.0 (by /u/dayouness)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+    else:
+        print(f"Error: {response.status_code}")
+        return 0
